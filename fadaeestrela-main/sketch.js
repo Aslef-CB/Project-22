3c77007bf9313844687292371ffd9ac2d995fbd3
Aslef <94656585+Aslef-CB@users.noreply.github.com>
2022-02-13 20:19:14 -0400

    Update sketch.js

--- fadaeestrela-main/sketch.js
+++ fadaeestrela-main/sketch.js
@@ -1,6 +1,6 @@
-var starImg,bgImg;
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
 var star, starBody;
-//criar variável para sprite de fada e imgFada
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -9,23 +9,28 @@ const Body = Matter.Body;
 
 function preload()
 {
-    starImg = loadImage("images/star.png");
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
-    //carregar animação de fada 
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
 }
 
 function setup() {
-    createCanvas(800, 750);
+	createCanvas(800, 750);
 
-    //escrever código para tocar o som vozFada
+	fairyVoice.play();
 
-    //criar sprite de fada e adicionar animação para fada
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
 
-    star = createSprite(650,30);
+	star = createSprite(650,30);
 	star.addImage(starImg);
 	star.scale = 0.2;
 
-    engine = Engine.create();
+
+	engine = Engine.create();
 	world = engine.world;
 
 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
@@ -33,4 +38,33 @@ function setup() {
 	
 	Engine.run(engine);
 
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if(keyCode === RIGHT_ARROW){
+           fairy.x = fairy.x + 20;
+	}
+	
+        if(keyCode === LEFT_ARROW){
+           fairy.x = fairy.x - 20;
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
 }
